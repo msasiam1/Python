f451f800dfc58cb956c0076b503ba73e4c7284ce
msasiam1 <msiam792@gmail.com>
2024-10-19 21:41:30 +0600

    (type function for variable type)

---
+++ findoutvariabletype.py
@@ -0,0 +1,17 @@
+#Here I will findout the variable types
+#1st method
+t="Hello Bangladesh"
+
+u=type(t)
+
+print(u)
+
+#2nd method
+
+y="This is Sanjid Alvi"
+
+print(type(y))
+
+n=16
+
+print(type(n))
